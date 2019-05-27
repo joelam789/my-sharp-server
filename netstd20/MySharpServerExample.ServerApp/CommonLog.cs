6d9c4384c72c786c808c94567ee353f032eaecbf
joelam789 <joelam789@yahoo.com.hk>
2019-05-27 16:23:10 +0800

    add example program for .net core version

---
+++ netstd20/MySharpServerExample.ServerApp/CommonLog.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MySharpServer.Common;
+using MySharpServer.Framework;
+
+namespace MySharpServerExample.ServerApp
+{
+    public static class CommonLog
+    {
+        static readonly ServerFormLogger m_Logger = null;
+
+        static CommonLog()
+        {
+            m_Logger = new ServerFormLogger();
+        }
+
+        public static IServerLogger GetLogger()
+        {
+            return m_Logger;
+        }
+
+        public static void Info(string msg)
+        {
+            if (m_Logger != null) m_Logger.Info(msg);
+        }
+
+        public static void Debug(string msg)
+        {
+            if (m_Logger != null) m_Logger.Debug(msg);
+        }
+
+        public static void Warn(string msg)
+        {
+            if (m_Logger != null) m_Logger.Warn(msg);
+        }
+
+        public static void Error(string msg)
+        {
+            if (m_Logger != null) m_Logger.Error(msg);
+        }
+    }
+
+    public class ServerFormLogger : ServerLogger
+    {
+
+        private void TryToSendLogToConsole(string msg)
+        {
+            try
+            {
+                Console.WriteLine("[" + DateTime.Now.ToString("HH:mm:ss") + "]" + msg);
+            }
+            catch { }
+        }
+
+        public override void Info(string msg)
+        {
+            base.Info(msg);
+            TryToSendLogToConsole("[INFO] " + msg);
+        }
+
+        public override void Debug(string msg)
+        {
+            base.Debug(msg);
+            TryToSendLogToConsole("[DEBUG] " + msg);
+        }
+
+        public override void Warn(string msg)
+        {
+            base.Warn(msg);
+            TryToSendLogToConsole("[WARN] " + msg);
+        }
+
+        public override void Error(string msg)
+        {
+            base.Error(msg);
+            TryToSendLogToConsole("[ERROR] " + msg);
+        }
+    }
+}
---
+++ netstd20/MySharpServerExample.ServerApp/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading;
+
+using NLog;
+using NLog.Config;
+
+using Newtonsoft.Json;
+
+using MySharpServer.Common;
+using MySharpServer.Framework;
+
+namespace MySharpServerExample.ServerApp
+{
+    class Program
+    {
+        static ServerNode m_ServerNode = null;
+
+        static ServerSetting m_InternalSetting = null;
+        static ServerSetting m_PublicSetting = null;
+
+        static string m_NodeName = "";
+        static string m_GroupName = "";
+
+        static string m_StorageName = "";
+
+        static List<string> m_ServiceFileNames = new List<string>();
+
+        static void Main(string[] args)
+        {
+            LogManager.Configuration = new XmlLoggingConfiguration($"{AppContext.BaseDirectory}/NLog.config");
+
+            Console.WriteLine("Loading app.config...");
+
+            var appSettings = ConfigurationManager.AppSettings;
+
+            var allKeys = appSettings.AllKeys;
+
+            foreach (var key in allKeys)
+            {
+                if (key == "InternalServer") m_InternalSetting = JsonConvert.DeserializeObject<ServerSetting>(appSettings[key]);
+                if (key == "PublicServer") m_PublicSetting = JsonConvert.DeserializeObject<ServerSetting>(appSettings[key]);
+
+                if (key == "NodeName") m_NodeName = appSettings[key];
+                if (key == "GroupName") m_GroupName = appSettings[key];
+                if (key == "ServerInfoStorageName") m_StorageName = appSettings[key];
+
+                if (key == "Services")
+                {
+                    var fileNames = appSettings[key].Split(',');
+                    m_ServiceFileNames.Clear();
+                    m_ServiceFileNames.AddRange(fileNames);
+                }
+            }
+
+            if (m_ServerNode == null)
+            {
+                m_ServerNode = new ServerNode(m_NodeName, m_GroupName, CommonLog.GetLogger());
+                m_ServerNode.SetServerInfoStorage(m_StorageName);
+                foreach (var svcFile in m_ServiceFileNames) m_ServerNode.AddLocalServiceFilepath(svcFile);
+            }
+
+            Console.WriteLine("Start server...");
+
+            if (m_ServerNode != null && !m_ServerNode.IsWorking())
+            {
+                m_ServerNode.Start(m_InternalSetting, m_PublicSetting);
+                Thread.Sleep(50);
+                if (m_ServerNode.IsWorking())
+                {
+                    CommonLog.Info("Server Started");
+                    CommonLog.Info("Internal URL: " + m_ServerNode.GetInternalAccessUrl());
+                    CommonLog.Info("Public URL: " + m_ServerNode.GetPublicAccessUrl());
+                }
+            }
+
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadLine();
+
+            Console.WriteLine("Stop server...");
+            if (m_ServerNode != null) m_ServerNode.Stop();
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
